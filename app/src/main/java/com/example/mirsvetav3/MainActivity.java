fb0a9c16f92abb6d7b00dd9e33015f844426bc36
Lion 2517 <lion2517@yahoo.com>
2023-01-31 21:06:12 +0500

    Мы спасли Мир

--- app/src/main/java/com/example/mirsvetav3/MainActivity.java
+++ app/src/main/java/com/example/mirsvetav3/MainActivity.java
@@ -19,11 +19,7 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new WebViewClient());
-//        myWebView.loadUrl("file:///android_asset/vite-app/dist/index.html");
         myWebView.loadUrl("file:///android_asset/Content/index.html");
-//        String unencodedHtml =
-//                "<html><body>'%23' is the percent code for ‘#‘ </body></html>";
-//        String encodedHtml = Base64.encodeToString(unencodedHtml.getBytes(), Base64.NO_PADDING);
-//        myWebView.loadData(encodedHtml, "text/html", "base64");
+
     }
 }
